998a86df09523333ae55cc310aef3c7ad8245ec8
FreifunkUFO <ufo@rund.freifunk.net>
2014-05-05 22:07:59 +0200

    batman-adv in trunk
    every drunken version of bad man

--- buildall.sh
+++ buildall.sh
@@ -21,6 +21,7 @@ echo "src-git luci2 http://git.openwrt.org/project/luci2/ui.git" >> feeds.conf
 echo "src-git cjdns git://github.com/lgierth/cjdns-openwrt.git" >> feeds.conf
 echo "src-git fastd git://git.metameute.de/lff/pkg_fastd" >> feeds.conf
 echo "src-git mwan3 git://github.com/Adze1502/mwan.git" >> feeds.conf
+echo "src-git batmanadv http://git.open-mesh.org/openwrt-feed-batman-adv.git" >> feeds.conf
 echo "src-git wbm git://github.com/battlemesh/battlemesh-packages.git" >> feeds.conf
 scripts/feeds update -a
 
