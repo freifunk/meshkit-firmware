30f93f8ed0eaea1db011206d061c02eee7cfe0e9
FreifunkUFO <ufo@rund.freifunk.net>
2014-06-24 17:17:15 +0200

    change date

--- buildall.sh
+++ buildall.sh
@@ -78,7 +78,7 @@ CONFIG_VERSIONOPT=y
 CONFIG_VERSION_DIST="OpenWrt"
 CONFIG_VERSION_NICK=""
 CONFIG_VERSION_NUMBER=""
-CONFIG_VERSION_REPO="http://rund.freifunk.net:8006/firmware/buildroots/ar71xx_generic_20140611/packages"
+CONFIG_VERSION_REPO="http://rund.freifunk.net:8006/firmware/buildroots/ar71xx_generic_20140624/packages"
 CONFIG_PACKAGE_collectd-mod-netlink=n
 CONFIG_PACKAGE_kmod-pcspkr=n
 EOF
