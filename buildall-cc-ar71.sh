b5dc7ba68d5130cbb50212399e2f31c8a09540f4
FreifunkUFO <ufo@rund.freifunk.net>
2015-12-01 10:57:57 +0100

    weniger alte Anweisungen wie batman-adv 2014.3

--- buildall-cc-ar71.sh
+++ buildall-cc-ar71.sh
@@ -2,13 +2,6 @@
 REMOTE=git://git.openwrt.org/15.05/openwrt.git
 TARGET=ar71xx
 #TARGET=x86
-# TARGET=ramips
-
-# in case of oxnas, also use oxnas remote site
-# REMOTE=git://gitorious.org/openwrt-oxnas/openwrt-oxnas.git
-# TARGET=oxnas
-
-# MAKEOPTS="-j4"
 
 # fail on errors
 set +e
@@ -30,22 +23,8 @@ echo "src-git fswebcam git://github.com/fsphil/fswebcam.git" >> feeds.conf
 echo "src-git solarfestival git://github.com/freifunk-leipzig/solarfestival-packages.git" >> feeds.conf
 #echo "src-git oldpackages http://git.openwrt.org/packages.git" >> feeds.conf
 
-scripts/feeds update -a
-
-# revert to batman-adv 2013.4.0
-# cd feeds/routing
-# git remote add github-routing git://github.com/openwrt-routing/packages.git
-# git fetch github-routing
-# git checkout -b batman-adv-backport
-# rm -r batman-adv
-# git checkout 89c2a8bb562412281d1ff070007be16d5a4d8f55 batman-adv
-# git commit -a -m "batman-adv: revert to 2013.4.0"
-# rm -r alfred
-# git checkout e2cfab7f287673b1d6854c59db6e710668d145f3 alfred
-# git commit -a -m "alread: revert to 2013.4.0"
-# cd ../..
-
 # create index and install all packages
+scripts/feeds update -a
 scripts/feeds update -i
 scripts/feeds install -a
 
@@ -93,6 +72,4 @@ set -e
 echo # make everything with
 echo #make $MAKEOPTS IGNORE_ERRORS=m V=99 BUILD_LOG=1
 echo #please do it within screen###
-echo make IGNORE_ERRORS=m V=99
-
-#echo dont forget to set batman-adv devel version git commit number manually if you need that
+echo make IGNORE_ERRORS=m
